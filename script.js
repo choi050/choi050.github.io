16c16027cac45d0042d0a00f952bd5440bac9cf1
choi050 <78195848+choi050@users.noreply.github.com>
2021-03-25 23:58:37 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 document.body.style.backgroundColor = "blue";
+//Editing background colour with js
 
 alert('Welcome to my page!');
+//Welcoming visitors with an alert message
 
 document.querySelector(".licontainer").style.border = "2px solid white";
+//adding style to the container using queryselector
