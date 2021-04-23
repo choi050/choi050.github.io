9c072b06efb0f3562201cbda6cd1562337c373e8
choi050 <78195848+choi050@users.noreply.github.com>
2021-04-23 08:58:06 -0400

    functions recipe page

---
+++ recipe/script.js
@@ -0,0 +1,10 @@
+// sandwich
+
+let meat1 = "ham";
+let veg1 = "lettuce";
+let bread1 = "wheat";
+
+function makeSandwich(meat, vegetable, bread){
+  let sandwich = meat + vegetable + bread;
+  return sandwich;
+}
