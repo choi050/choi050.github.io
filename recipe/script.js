e7da27d5d8a4b6c7caf36d43d21e3eb477f30537
choi050 <78195848+choi050@users.noreply.github.com>
2021-04-23 15:17:02 -0400

    finished recipes

--- recipe/script.js
+++ recipe/script.js
@@ -1,10 +1,42 @@
-// sandwich
 
-let meat1 = "ham";
-let veg1 = "lettuce";
-let bread1 = "wheat";
+ $("#button1").click(function() {
+   $(this).effect( "shake" );
+ });
 
-function makeSandwich(meat, vegetable, bread){
-  let sandwich = meat + vegetable + bread;
-  return sandwich;
-}
+ $( function() {
+     $( "#draggable1" ).draggable();
+   } );
+
+   $( function() {
+       $( "#draggable2" ).draggable();
+     } );
+
+     $( function() {
+         $( "#draggable3" ).draggable();
+       } );
+
+       $( function() {
+           $( "#draggable4" ).draggable();
+         } );
+
+         $( function() {
+             $( "#draggable5" ).draggable();
+           } );
+
+           $( function() {
+               $( "#draggable6" ).draggable();
+             } );
+
+             $( function() {
+                 $( "#draggable7" ).draggable();
+               } );
+
+               $( function() {
+                   $( "#draggable8" ).draggable();
+                 } );
+                 $( function() {
+                     $( "#draggable9" ).draggable();
+                   } );
+                   $( function() {
+                       $( "#draggable10" ).draggable();
+                     } );
