f94646ca6ed8c3cf94b91b07598dc52623de9fda
choi050 <78195848+choi050@users.noreply.github.com>
2021-05-06 13:59:25 -0400

    finish final project

--- final/resolution/script.js
+++ final/resolution/script.js
@@ -12,20 +12,19 @@ $('.play').click(function(){
     }
 });
 
-
-
 //button 1 stuff
+
 document.getElementById("btn1").addEventListener("click", function(){
-    var timeleft = 3;
+    var timeleft = 5;
 
     var downloadTimer = setInterval(function function1(){
-    document.getElementById("countdown1").innerHTML = timeleft +
-    "&nbsp"+"seconds to rising action";
+    document.getElementById("countdown").innerHTML = timeleft +
+    "&nbsp";
 
     timeleft -= 1;
-    if(timeleft <= 0){
+    if(timeleft < 0){
         clearInterval(downloadTimer);
-        document.getElementById("countdown1").innerHTML = "Time is up!"
+        document.getElementById("countdown").innerHTML = "✓"
     }
     }, 1000);
 
@@ -33,234 +32,261 @@ document.getElementById("btn1").addEventListener("click", function(){
 });
 
 function myFunction() {
-  setTimeout(function(){ alert("Time for the rising action"); }, 3100);
+  setTimeout(function(){ alert("Time for the action to rise!"); }, 5100);
 
   var element = document.getElementById("activate");
   element.classList.add("active");
 }
 
-document.getElementById("countdown1").style.background = "black";
-document.getElementById("countdown1").style.color = "lime";
-document.getElementById("countdown1").style.borderRadius = "5px";
-document.getElementById("countdown1").style.boxShadow = "-1px -1px 1px #000, 1px 1px 1px #fff";
-
-
-
-
+document.getElementById("countdown").style.background = "black";
+document.getElementById("countdown").style.color = "#F0F0A1";
+document.getElementById("countdown").style.borderRadius = "50%";
+document.getElementById("countdown").style.width = "50px";
+document.getElementById("countdown").style.fontFamily = "Helvetica";
 
 // button2 from here
 
 document.getElementById("btn2").addEventListener("click", function(){
-    var timeleft = 90;
+    var timeleft = 5;
 
     var downloadTimer = setInterval(function function2(){
-    document.getElementById("countdown2").innerHTML = timeleft +
-    "&nbsp"+"seconds to rising action";
+    document.getElementById("countdown").innerHTML = timeleft +
+    "&nbsp";
 
     timeleft -= 1;
-    if(timeleft <= 0){
+    if(timeleft < 0){
         clearInterval(downloadTimer);
-        document.getElementById("countdown2").innerHTML = "Time is up!"
+        document.getElementById("countdown").innerHTML = "✓"
     }
     }, 1000);
 
     console.log(countdown);
 });
-
-function myFunction2() {
-  setTimeout(function(){ alert("Time for the rising action!"); }, 9100);
-
-  var element = document.getElementById("activate");
-  element.classList.add("active");
-}
-
-document.getElementById("countdown2").style.background = "black";
-document.getElementById("countdown2").style.color = "lime";
-document.getElementById("countdown2").style.borderRadius = "5px";
-document.getElementById("countdown2").style.boxShadow = "-1px -1px 1px #000, 1px 1px 1px #fff";
 
 // button3 from here
 
 document.getElementById("btn3").addEventListener("click", function(){
-    var timeleft = 90;
+    var timeleft = 5;
 
     var downloadTimer = setInterval(function function3(){
-    document.getElementById("countdown2").innerHTML = timeleft +
-    "&nbsp"+"seconds to rising action";
+    document.getElementById("countdown").innerHTML = timeleft +
+    "&nbsp";
 
     timeleft -= 1;
-    if(timeleft <= 0){
+    if(timeleft < 0){
         clearInterval(downloadTimer);
-        document.getElementById("countdown2").innerHTML = "Time is up!"
+        document.getElementById("countdown").innerHTML = "✓"
     }
     }, 1000);
 
     console.log(countdown);
 });
-
-function myFunction3() {
-  setTimeout(function(){ alert("Time for the climax!"); }, 91000);
-
-  var element = document.getElementById("activate");
-  element.classList.add("active");
-}
-
-document.getElementById("countdown2").style.background = "black";
-document.getElementById("countdown2").style.color = "lime";
-document.getElementById("countdown2").style.borderRadius = "5px";
-document.getElementById("countdown2").style.boxShadow = "-1px -1px 1px #000, 1px 1px 1px #fff";
 
 // button4 from here
 
 document.getElementById("btn4").addEventListener("click", function(){
-    var timeleft = 90;
+    var timeleft = 5;
 
     var downloadTimer = setInterval(function function4(){
-    document.getElementById("countdown2").innerHTML = timeleft +
-    "&nbsp"+"seconds to rising action";
+    document.getElementById("countdown").innerHTML = timeleft +
+    "&nbsp";
 
     timeleft -= 1;
-    if(timeleft <= 0){
+    if(timeleft < 0){
         clearInterval(downloadTimer);
-        document.getElementById("countdown2").innerHTML = "Time is up!"
+        document.getElementById("countdown").innerHTML = "✓"
     }
     }, 1000);
 
     console.log(countdown);
 });
-
-function myFunction4() {
-  setTimeout(function(){ alert("Time for the climax!"); }, 91000);
-
-  var element = document.getElementById("activate");
-  element.classList.add("active");
-}
-
-document.getElementById("countdown2").style.background = "black";
-document.getElementById("countdown2").style.color = "lime";
-document.getElementById("countdown2").style.borderRadius = "5px";
-document.getElementById("countdown2").style.boxShadow = "-1px -1px 1px #000, 1px 1px 1px #fff";
 
 // button5 from here
 
 document.getElementById("btn5").addEventListener("click", function(){
-    var timeleft = 90;
+    var timeleft = 5;
 
     var downloadTimer = setInterval(function function5(){
-    document.getElementById("countdown2").innerHTML = timeleft +
-    "&nbsp"+"seconds to rising action";
+    document.getElementById("countdown").innerHTML = timeleft +
+    "&nbsp";
 
     timeleft -= 1;
-    if(timeleft <= 0){
+    if(timeleft < 0){
         clearInterval(downloadTimer);
-        document.getElementById("countdown2").innerHTML = "Time is up!"
+        document.getElementById("countdown").innerHTML = "✓"
     }
     }, 1000);
 
     console.log(countdown);
 });
-
-function myFunction5() {
-  setTimeout(function(){ alert("Time for the climax!"); }, 91000);
-
-  var element = document.getElementById("activate");
-  element.classList.add("active");
-}
-
-document.getElementById("countdown2").style.background = "black";
-document.getElementById("countdown2").style.color = "lime";
-document.getElementById("countdown2").style.borderRadius = "5px";
-document.getElementById("countdown2").style.boxShadow = "-1px -1px 1px #000, 1px 1px 1px #fff";
 
 // button6 from here
 
 document.getElementById("btn6").addEventListener("click", function(){
-    var timeleft = 90;
+    var timeleft = 5;
 
     var downloadTimer = setInterval(function function6(){
-    document.getElementById("countdown2").innerHTML = timeleft +
-    "&nbsp"+"seconds to rising action";
+    document.getElementById("countdown").innerHTML = timeleft +
+    "&nbsp";
 
     timeleft -= 1;
-    if(timeleft <= 0){
+    if(timeleft < 0){
         clearInterval(downloadTimer);
-        document.getElementById("countdown2").innerHTML = "Time is up!"
+        document.getElementById("countdown").innerHTML = "✓"
     }
     }, 1000);
 
     console.log(countdown);
 });
-
-function myFunction6() {
-  setTimeout(function(){ alert("Time for the climax!"); }, 91000);
-
-  var element = document.getElementById("activate");
-  element.classList.add("active");
-}
-
-document.getElementById("countdown2").style.background = "black";
-document.getElementById("countdown2").style.color = "lime";
-document.getElementById("countdown2").style.borderRadius = "5px";
-document.getElementById("countdown2").style.boxShadow = "-1px -1px 1px #000, 1px 1px 1px #fff";
 
 // button7 from here
 
 document.getElementById("btn7").addEventListener("click", function(){
-    var timeleft = 90;
+    var timeleft = 5;
 
     var downloadTimer = setInterval(function function7(){
-    document.getElementById("countdown2").innerHTML = timeleft +
-    "&nbsp"+"seconds to rising action";
+    document.getElementById("countdown").innerHTML = timeleft +
+    "&nbsp";
 
     timeleft -= 1;
-    if(timeleft <= 0){
+    if(timeleft < 0){
         clearInterval(downloadTimer);
-        document.getElementById("countdown2").innerHTML = "Time is up!"
+        document.getElementById("countdown").innerHTML = "✓"
     }
     }, 1000);
 
     console.log(countdown);
 });
-
-function myFunction7() {
-  setTimeout(function(){ alert("Time for the climax!"); }, 91000);
-
-  var element = document.getElementById("activate");
-  element.classList.add("active");
-}
-
-document.getElementById("countdown2").style.background = "black";
-document.getElementById("countdown2").style.color = "lime";
-document.getElementById("countdown2").style.borderRadius = "5px";
-document.getElementById("countdown2").style.boxShadow = "-1px -1px 1px #000, 1px 1px 1px #fff";
 
 // button8 from here
 
 document.getElementById("btn8").addEventListener("click", function(){
-    var timeleft = 90;
+    var timeleft = 5;
 
     var downloadTimer = setInterval(function function8(){
-    document.getElementById("countdown2").innerHTML = timeleft +
-    "&nbsp"+"seconds to rising action";
+    document.getElementById("countdown").innerHTML = timeleft +
+    "&nbsp";
 
     timeleft -= 1;
-    if(timeleft <= 0){
+    if(timeleft < 0){
         clearInterval(downloadTimer);
-        document.getElementById("countdown2").innerHTML = "Time is up!"
+        document.getElementById("countdown").innerHTML = "✓"
     }
     }, 1000);
 
     console.log(countdown);
 });
 
-function myFunction8() {
-  setTimeout(function(){ alert("Time for the climax!"); }, 91000);
+// button9 from here
 
-  var element = document.getElementById("activate");
-  element.classList.add("active");
-}
+document.getElementById("btn9").addEventListener("click", function(){
+    var timeleft = 5;
 
-document.getElementById("countdown2").style.background = "black";
-document.getElementById("countdown2").style.color = "lime";
-document.getElementById("countdown2").style.borderRadius = "5px";
-document.getElementById("countdown2").style.boxShadow = "-1px -1px 1px #000, 1px 1px 1px #fff";
+    var downloadTimer = setInterval(function function9(){
+    document.getElementById("countdown").innerHTML = timeleft +
+    "&nbsp";
+
+    timeleft -= 1;
+    if(timeleft < 0){
+        clearInterval(downloadTimer);
+        document.getElementById("countdown").innerHTML = "✓"
+    }
+    }, 1000);
+
+    console.log(countdown);
+});
+
+// button10 from here
+
+document.getElementById("btn10").addEventListener("click", function(){
+    var timeleft = 5;
+
+    var downloadTimer = setInterval(function function10(){
+    document.getElementById("countdown").innerHTML = timeleft +
+    "&nbsp";
+
+    timeleft -= 1;
+    if(timeleft < 0){
+        clearInterval(downloadTimer);
+        document.getElementById("countdown").innerHTML = "✓"
+    }
+    }, 1000);
+
+    console.log(countdown);
+});
+
+// button11 from here
+
+document.getElementById("btn11").addEventListener("click", function(){
+    var timeleft = 5;
+
+    var downloadTimer = setInterval(function function11(){
+    document.getElementById("countdown").innerHTML = timeleft +
+    "&nbsp";
+
+    timeleft -= 1;
+    if(timeleft < 0){
+        clearInterval(downloadTimer);
+        document.getElementById("countdown").innerHTML = "✓"
+    }
+    }, 1000);
+
+    console.log(countdown);
+});
+
+// button 12 from here
+
+document.getElementById("btn12").addEventListener("click", function(){
+    var timeleft = 5;
+
+    var downloadTimer = setInterval(function function12(){
+    document.getElementById("countdown").innerHTML = timeleft +
+    "&nbsp";
+
+    timeleft -= 1;
+    if(timeleft < 0){
+        clearInterval(downloadTimer);
+        document.getElementById("countdown").innerHTML = "✓"
+    }
+    }, 1000);
+
+    console.log(countdown);
+});
+
+// button13 from here
+
+document.getElementById("btn13").addEventListener("click", function(){
+    var timeleft = 5;
+
+    var downloadTimer = setInterval(function function13(){
+    document.getElementById("countdown").innerHTML = timeleft +
+    "&nbsp";
+
+    timeleft -= 1;
+    if(timeleft < 0){
+        clearInterval(downloadTimer);
+        document.getElementById("countdown").innerHTML = "✓"
+    }
+    }, 1000);
+
+    console.log(countdown);
+});
+
+// button14 from here
+
+document.getElementById("btn14").addEventListener("click", function(){
+    var timeleft = 5;
+
+    var downloadTimer = setInterval(function function14(){
+    document.getElementById("countdown").innerHTML = timeleft +
+    "&nbsp";
+
+    timeleft -= 1;
+    if(timeleft < 0){
+        clearInterval(downloadTimer);
+        document.getElementById("countdown").innerHTML = "✓"
+    }
+    }, 1000);
+
+    console.log(countdown);
+});
--- final/script.js
+++ final/script.js
@@ -1,4 +1,10 @@
-$( function() {
-  var $winHeight = $( window ).height()
-  $( '.container' ).height( $winHeight );
-});
+function on() {
+  document.getElementById("overlay").style.display = "block";
+}
+
+function off() {
+  document.getElementById("overlay").style.display = "none";
+}
+
+document.getElementById("off").style.width = "50px";
+document.getElementById("off").style.height = "50px";
